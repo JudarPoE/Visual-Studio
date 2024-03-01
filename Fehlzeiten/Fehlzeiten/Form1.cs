16416a20a86c8ca4df3b85a261c88e81cefe56dd
Judar <118455327+JudarPoE@users.noreply.github.com>
2024-03-01 16:32:14 +0100

    Add files via upload

---
+++ Fehlzeiten/Fehlzeiten/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fehlzeiten
+{
+    public partial class Form1 : Form
+    {
+        List<Schüler> PersonenListe = new List<Schüler>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_SchülerAnlegen(object sender, EventArgs e)
+        {
+            if (TextBox_Name_Schülerverwalten.Text != "" && TextBox_Vorname_Schülerverwalten.Text != "")
+            {
+                Schüler einSchüler = new Schüler(TextBox_Name_Schülerverwalten.Text, TextBox_Vorname_Schülerverwalten.Text, Convert.ToDateTime(DateTimePicker_Schülerverwalten.Text), Convert.ToDouble(NumericUpDown_Anfahrt_Schülerverwalten.Text), CheckBox_Buskarte_Schülerverwalten.Checked);
+                PersonenListe.Add(einSchüler);
+
+                TextBox_Name_Schülerverwalten.Text = "";
+                TextBox_Vorname_Schülerverwalten.Text = "";
+                DateTimePicker_Schülerverwalten.Text = "";
+                NumericUpDown_Anfahrt_Schülerverwalten.Text = "";
+                CheckBox_Buskarte_Schülerverwalten.Checked = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // ComboBox mit Schülern füllen
+            ComboBox_Schüler_Fehlzeitenverwalten.DataSource = PersonenListe;
+            ComboBox_Schüler_Fehlzeitenverwalten.DisplayMember = "Name"; // Hier "Name" durch den tatsächlichen Eigenschaftsnamen ersetzen
+        }
+
+        private void ComboBox_Schüler_Fehlzeitenverwalten_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Hier können Sie bei Bedarf weitere Aktionen ausführen, wenn ein anderer Schüler ausgewählt wird
+        }
+    }
+}
---
+++ Fehlzeiten/Fehlzeiten/Lehrer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fehlzeiten
+{
+    public class Lehrer 
+    {
+        public string Fach1 { get; set; }
+        public string Fach2 { get; set; }
+        public DateTime Eintrittsdatum { get; set; }        
+    }
+}
+
+        
+
+
